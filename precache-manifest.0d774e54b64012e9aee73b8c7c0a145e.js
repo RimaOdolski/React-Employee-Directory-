072dca129745ec64ccb31bf139b2d151633b7149
Rima O <rymaothmani@gmail.com>
2020-05-05 21:33:42 -0500

    Updates

---
+++ precache-manifest.0d774e54b64012e9aee73b8c7c0a145e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36287cd122164ab6433b58d83273ebf5",
+    "url": "/React-Employee-Directory-/index.html"
+  },
+  {
+    "revision": "b36e1d86b37fb7fec7b1",
+    "url": "/React-Employee-Directory-/static/css/main.59fb3cce.chunk.css"
+  },
+  {
+    "revision": "ba5ed9bd550b241734ed",
+    "url": "/React-Employee-Directory-/static/js/2.c7aeff9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory-/static/js/2.c7aeff9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36e1d86b37fb7fec7b1",
+    "url": "/React-Employee-Directory-/static/js/main.4025ab1c.chunk.js"
+  },
+  {
+    "revision": "fe0103df8be9b68b3764",
+    "url": "/React-Employee-Directory-/static/js/runtime-main.d0244ea4.js"
+  }
+]);
